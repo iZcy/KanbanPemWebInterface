eb9c6c65c1b3a9cc2865d128de75f6c2f85f313b
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-11-26 20:09:49 +0700

    fix: bug layout

--- src/app/(pages)/(restricted)/layout.tsx
+++ src/app/(pages)/(restricted)/layout.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import apiRoute from "@/api/routes";
-import { AccountData, useCredentialsContext } from "@/contexts/CredentialsContext";
+import {
+  AccountData,
+  useCredentialsContext
+} from "@/contexts/CredentialsContext";
 import { useToasterContext } from "@/contexts/ToasterContext";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const RootLayout = ({
   children
@@ -17,24 +20,30 @@ const RootLayout = ({
   const credentialsController = useCredentialsContext();
   const { setIsLoading } = toasterController.confirmationToast;
 
-  useEffect(() => {
-    axios.get(apiRoute.auth.loginRoute, {
-      withCredentials: true
-    }).then((res) => {
-      const data = res.data.data as AccountData;
-      console.log(data)
-      if (!data.role) router.replace("/auth");
-      else credentialsController.setAccData(data);
+  const setIsLoadingRef = useRef(setIsLoading);
+  const setAccDataRef = useRef(credentialsController.setAccData);
 
-      setIsLoading(false);
-    }).catch((error) => {
-      console.error("Failed to fetch authentication status:", error);
-      router.replace("/auth");
-    }).finally(() => {
-      setIsLoading(false); // Stop loading on
-    });
-    
-  }, []);
+  useEffect(() => {
+    axios
+      .get(apiRoute.auth.loginRoute, {
+        withCredentials: true
+      })
+      .then((res) => {
+        const data = res.data.data as AccountData;
+        console.log(data);
+        if (!data.role) router.replace("/auth");
+        else setAccDataRef.current(data);
+
+        setIsLoadingRef.current(false);
+      })
+      .catch((error) => {
+        console.error("Failed to fetch authentication status:", error);
+        router.replace("/auth");
+      })
+      .finally(() => {
+        setIsLoadingRef.current(false); // Stop loading on
+      });
+  }, [router]);
 
   if (!toasterController.confirmationToast.isLoading) {
     return children;
